f1a1481d653dca6d71a562d4bd7a57febeafcede
elhachimi <wael@unik.ma>
2015-01-18 12:41:09 +0000

    add category model

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe Article do
+    it "orders them in reverse chronologically"
+    #   post = Post.create!
+    #   comment1 = post.comments.create!(:body => "first comment")
+    #   comment2 = post.comments.create!(:body => "second comment")
+    #   expect(post.reload.comments).to eq([comment2, comment1])
+    # end
+end
