436d9c72072fe4f057e37f62c0aa6e9becfe679e
elhachimi <wael@unik.ma>
2015-01-18 12:43:04 +0000

    add test for article and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117165330) do
+ActiveRecord::Schema.define(version: 20150118124036) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,5 @@
-require "spec_helper"
+require 'rails_helper'
 
-describe Article do
-    it "orders them in reverse chronologically"
-    #   post = Post.create!
-    #   comment1 = post.comments.create!(:body => "first comment")
-    #   comment2 = post.comments.create!(:body => "second comment")
-    #   expect(post.reload.comments).to eq([comment2, comment1])
-    # end
+RSpec.describe Article, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
